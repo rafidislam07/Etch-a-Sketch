7bdd68e4658b559f2c11ae83560f1076230d2c5a
Rafid Islam <rafidislam529@gmail.com>
2024-07-08 21:08:34 +0600

    project completed (grid, cells, slider, buttonprompt)

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ numOfSquares = 16
 const sketchArea = document.querySelector("#sketch-area")
 sketchArea.style.height = `${gridSize}px` 
 sketchArea.style.width = `${gridSize}px` 
+
 const slider = document.querySelector("#slider")
 const sliderValue = document.querySelector("#slider-value")
 sliderValue.textContent = `${numOfSquares} x ${numOfSquares} resolution`
 
+const btn = document.querySelector("#btn")
+
 function setBGcolor() {
-    this.style.backgroundColor = "black"
+    this.style.backgroundColor = "rebeccapurple"
 }
 
 function createGridCells(numOfSquares) {
@@ -30,12 +33,28 @@ function removeGridCells() {
     }
 }
 
+function promptResolution() {
+    resValue = prompt(
+        "Select Resolution"
+    )
+    if (resValue > 100) {
+        alert("100 is the max boss")
+    }
+    else {
+    let txt2 = `${resValue} x ${resValue} resolution`
+    sliderValue.textContent = txt2
+    removeGridCells()
+    createGridCells(resValue)
+    }
+}
+
 slider.oninput = function() {
     let txt = `${this.value} x ${this.value} resolution`
-    sliderValue.innerHTML = txt
+    sliderValue.textContent = txt
     removeGridCells()
     createGridCells(this.value)
 }
 
+btn.addEventListener("click", promptResolution)
 createGridCells(16)
 
